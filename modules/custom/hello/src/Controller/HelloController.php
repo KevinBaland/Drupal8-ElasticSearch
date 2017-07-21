107e6d12ea9fd9b3b1bf3a0e9f6c28ee85404d17
kevinbd <baland.kevin@yahoo.fr>
2017-07-21 10:58:14 +0200

    Test module

---
+++ modules/custom/hello/src/Controller/HelloController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\hello\Controller\HelloController.
+ */
+namespace Drupal\hello\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+
+class HelloController extends ControllerBase {
+  public function content() {
+    return array(
+        '#markup' => '' . t('Hello there! ') . '',
+    );
+  }
+}
