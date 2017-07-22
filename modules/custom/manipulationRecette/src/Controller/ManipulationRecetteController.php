c0c9cf70dc252b86262b831036b1809753b3b62f
kevinbd <baland.kevin@yahoo.fr>
2017-07-22 16:39:14 +0200

    Test Pagination

--- modules/custom/manipulationRecette/src/Controller/ManipulationRecetteController.php
+++ modules/custom/manipulationRecette/src/Controller/ManipulationRecetteController.php
@@ -10,6 +10,8 @@ use Drupal\node\Entity\Node;
 
 
 class ManipulationRecetteController extends ControllerBase {
+    
+    //Affiche une recette
     public function content() {
      
         //Recuperation de la listes des nodes ids
@@ -27,11 +29,34 @@ class ManipulationRecetteController extends ControllerBase {
         return $render;
     }
     
+    //Affichage de toutes les Recettes
     public function view_all() {
      
         //Recuperation de la listes des nodes ids.
         $nodeIds = \Drupal::entityQuery('node')
         ->condition('type', 'recette')
+        ->execute();
+        
+        //Recuperation des nodes correspondant a la liste des nodes ids.
+        $nodes = Node::loadMultiple($nodeIds);
+        
+        //Tableau contenant la vue de chaque recette.
+        $render_array = [];
+        
+        foreach ($nodes as $node) {
+            $render_array[] = node_view($node);
+        }
+        
+        return $render_array;
+    }
+    
+    //Affiche 2 Recette par page.
+    public function page($page){
+        
+        //Recuperation de la listes des nodes ids.
+        $nodeIds = \Drupal::entityQuery('node')
+        ->condition('type', 'recette')
+        ->range(($page-1)*2,2)
         ->execute();
         
         //Recuperation des nodes correspondant a la liste des nodes ids.
