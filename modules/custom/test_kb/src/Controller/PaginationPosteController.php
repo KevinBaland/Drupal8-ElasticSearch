0578a8f444339e9212c0c0b935433bf50749d09f
kevinbd <baland.kevin@yahoo.fr>
2017-07-24 14:14:47 +0200

    Affichage par Departement (3 postes par pages)

---
+++ modules/custom/test_kb/src/Controller/PaginationPosteController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\test_kb\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
+
+/**
+ * Description of PaginationController
+ *
+ * @author kevin
+ */
+class PaginationPosteController extends ControllerBase{
+    
+    public function pagination($dept,$page){
+
+        //Recuperation de la liste des ids des Term correspondant a Departement
+        $query = \Drupal::entityQuery('taxonomy_term');
+        $query->condition('vid', "departement");
+        $query->condition('name', $dept);
+        $tids = $query->execute();
+        
+        //Recuperation de la listes des nodes ids.
+        $nodeIds = \Drupal::entityQuery('node')
+        ->condition('type', 'poste')
+        ->range(($page-1)*3,3)
+        ->condition('field_dept',$tids)
+        ->execute();
+        
+        //var_dump($nodeIds);exit;
+        
+        //Recuperation des nodes correspondant a la liste des nodes ids.
+        $nodes = Node::loadMultiple($nodeIds);
+        
+        //Tableau contenant la vue de chaque recette.
+        $render_array = [];
+        
+        foreach ($nodes as $node) {
+            $render_array[] = node_view($node);
+        }
+        
+        return $render_array;
+    }
+}
